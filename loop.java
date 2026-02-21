a99f8920932d6596601dbbdb1944942a39086262
Bhavnaom44 <bhavnaom36@gmail.com>
2026-02-21 16:55:07 +0500

    Add loop to print roll numbers from 1 to 30

---
+++ loop.java
@@ -0,0 +1,18 @@
+package bhavnaapplication;
+
+public class loop {
+ public static void main(String[] args) {
+	 
+		     
+		        System.out.println("University Name: Mehran University of Engineering & Technology");
+		        System.out.println("Department Name: Computer System");
+		        System.out.println();
+
+		       
+		        for (int i = 1; i <= 30; i++) {
+		            System.out.println("Roll No: " + i);
+		        }
+		    }
+		}
+
+	 
