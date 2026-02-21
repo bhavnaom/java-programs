b248b76934379ec536c31126f3d79b653f713734
Bhavnaom44 <bhavnaom36@gmail.com>
2026-02-21 17:01:35 +0500

    Add Store class to calculate total cost of products

---
+++ String.java
@@ -0,0 +1,18 @@
+class Store {
+
+    public static void main(String[] args) {
+
+        String storeName = "ABC Store";
+        String productName = "Pen";
+        int price = 10;
+        int quantity = 5;
+
+        int totalCost = price * quantity;
+
+        System.out.println("Store Name: " + storeName);
+        System.out.println("Product Name: " + productName);
+        System.out.println("Price: " + price);
+        System.out.println("Quantity: " + quantity);
+        System.out.println("Total Cost: " + totalCost);
+    }
+}
